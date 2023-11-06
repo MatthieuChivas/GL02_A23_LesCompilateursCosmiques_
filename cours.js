6aa884a030b0b13bf07c08f59b1023899a0a9967
MatthieuChivas <117097696+MatthieuChivas@users.noreply.github.com>
2023-11-07 00:35:28 +0100

    Changement majeur structure

--- cours.js
+++ cours.js
@@ -1,10 +1,16 @@
 class Cours {
     constructor(nom,creaneau){
-        this.creneau=creaneau;
+        this.creneau= new Array();
         this.nom = nom;
     }
     afficherCour(){
         console.log("J'ai cours");    
+    }
+    ajouterCreneau(creneau){
+        this.creneau.push(creneau);
+    }
+    getCreneaux(){
+        return this.creneau;
     }
 }
 
