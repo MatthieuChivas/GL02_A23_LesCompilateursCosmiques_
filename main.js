94118371ede5ad9942792283e3a733c2a5bbafa0
adrienlau <118347287+adrienlau@users.noreply.github.com>
2023-12-08 18:37:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -73,232 +73,34 @@ class Main{
 
     async menuSalleLibrePourUnCreaneau(){
 
-        // Déclaration des tableaux
-
         let sallesDispo = new Array(6).fill(null).map(() => 
-            new Array(24).fill(null).map(() => new Array(200).fill(null))
+        new Array(24).fill(null).map(() => new Array(200).fill(null))
         );
-        let listeCreneaux = ["8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", 
-        "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", 
-        "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00" ];
-        let listeJours = ["L", "M", "ME", "J", "V", "S"];
-        let sallesDispoMobile = new Array(200);
         
-        // Déclaration des variables
-
-        let i; 
-        let i2;
-        let j; 
-        let k;
-        let kMobile;
-        let iChoix;
-        let jChoix;
-        let j2Choix;
-        let kChoix;
-        let infoValide;
-        let creneauValide;
-        let sallePresente;
-        let nSallesDispo=0;
-
-        // -----------------------------------------------------------
-
-        const fs = require('fs');
-
-        const filePath = './Data/data.txt';
+        let listeCreneaux;
+        let cChoix;
+        let hdChoix;
+        let hfChoix;
+        let salle;
         
-        const texte = fs.readFileSync(filePath, 'utf8');
-            
-        /*const texte = `
-        +AP03
-        1,D1,P=24,H=V 9:00-12:00,F1,S=B103//
-        1,D2,P=25,H=V 13:00-16:00,F1,S=B103//
-        +BI01
-        1,C1,P=25,H=ME 8:00-10:00,F1,S=P101//
-        1,D1,P=49,H=ME 10:00-12:00,F1,S=P104//
-        +BI02
-        1,C1,P=35,H=J 8:00-10:00,F1,S=B103//
-        1,D1,P=25,H=J 10:00-12:00,F1,S=B102//
-        `;*/
-
-        // Transformer la chaîne en tableau de caractères
         
-        const caracteres = Array.from(texte);
-        console.log(texte);
-        console.log (caracteres);
-
-
-        let infos = new Array(4).fill(null);
-        for (i2 = 0; i2 < caracteres.length; i2++){
-            if (caracteres[i2]=='H'){
-                infos[0]=caracteres[i2+2];
-                if ((caracteres[i2+2]=='M')&&(caracteres[i2+3]=='E')){
-                    infos[0]='ME'
-                }
-            }
-            if (caracteres[i2]=='-'){
-                if (caracteres[i2-5]==' '){
-                    infos[1]=caracteres[i2-4]+caracteres[i2-3]+caracteres[i2-2]+caracteres[i2-1];
-                }else{
-                    infos[1]=caracteres[i2-5]+caracteres[i2-4]+caracteres[i2-3]+caracteres[i2-2]+caracteres[i2-1];
-                }
-                if (caracteres[i2+5]==','){
-                    infos[2]=caracteres[i2+1]+caracteres[i2+2]+caracteres[i2+3]+caracteres[i2+4];
-                }else{
-                    infos[2]=caracteres[i2+1]+caracteres[i2+2]+caracteres[i2+3]+caracteres[i2+4]+caracteres[i2+5];
-                }
-            }
-            if ((caracteres[i2]=='S')&&(caracteres[i2+1]=='=')){
-                infos[3]=caracteres[i2+2]+caracteres[i2+3]+caracteres[i2+4]+caracteres[i2+5]
-            }
-            if ((caracteres[i2]=='/')&&(infos[0]!=null)&&(infos[1]!=null)&&(infos[2]!=null)&&(infos[3]!=null)){
-                infoValide=0;
-                for (i = 0; i < 6; i++){
-                    if (infos[0]==listeJours[i]){
-                        infoValide=infoValide+1;
-                        iChoix=i;
-                    }
-                }
-                for (i = 0; i < 24; i++){
-                    if (infos[1]==listeCreneaux[i]){
-                        infoValide=infoValide+1;
-                        jChoix=i;
-                    }
-                }
-                for (i = 0; i < 25; i++){
-                    if (infos[2]==listeCreneaux[i]){
-                        infoValide=infoValide+1;
-                        j2Choix=i;
-                    }
-                }
-                if (infoValide==3){
-                    k=0;
-                    for (i = jChoix; i < j2Choix; i++){
-                        while (sallesDispo[iChoix][i][k]!=null){
-                            k=k+1
-                        }
-                        sallesDispo[iChoix][i][k]=infos[3];
-                    }
-                }
-                for (j = 0; j < 4; j++){
-                    infos[j]=null
-                }
+        for (i=0;i<this.universite.cours.creneau.length;i++){
+            for (j=0;j<this.universite.cours.creneau.horaire.length;j++){
+                //sallesDispo[i][j][k]=this.universite.cours.creneau.salle
+                listeJours.listeCreneaux.salle=
             }
         }
-
-
-
-
-
-
-
-
-
-        // -----------------------------------------------------------
-
-        // On demande le jour et les heures de début et de fin du créneau
-        // On stocke leur rang dans les tableaux listeJours et listeCreneaux dans des variables iChoix, jChoix et j2choix
-        // Par exemple, si le jour saisi est L, iChoix prend la valeur 0
-
-        console.log(`Veuillez renseigner un créneau `);
-        infoValide=0;
-        while (infoValide==0){
-            const choixJour = await this.questionAsync("Quel jour ? (L pour Lundi, M pour Mardi, ME pour Mercredi...) ");
-            for (i = 0; i < 6; i++){
-                if (choixJour==listeJours[i]){
-                    infoValide=1;
-                    iChoix=i;
-                }
-            }
-            if (infoValide==0){
-                console.log(`Jour non valide`);
-            }
-        }
-        creneauValide=0;
-        while (creneauValide==0){
-            infoValide=0;
-            while (infoValide==0){
-                const choixHeureDebut = await this.questionAsync("Quelle heure de début ? (8:00, 8:30, 9:00...) ");
-                for (i = 0; i < 24; i++){
-                    if (choixHeureDebut==listeCreneaux[i]){
-                        infoValide=1;
-                        jChoix=i;
-                    }
-                }
-                if (infoValide==0){
-                    console.log(`Horaire non valide`);
-                }
-            }
-
-            infoValide=0;
-            while (infoValide==0){
-                const choixHeureFin = await this.questionAsync("Quelle heure de fin ? (8:00, 8:30, 9:00...) ");
-                for (i = 1; i < 25; i++){
-                    if (choixHeureFin==listeCreneaux[i]){
-                        infoValide=1;
-                        j2Choix=i-1;
-                    }
-                }
-                if (infoValide==0){
-                    console.log(`Horaire non valide`);
-                }
-            }
-            if(jChoix>j2Choix){
-                console.log(`Créneau non valide : durée du créneau nulle ou négative`);
-            }else{
-                creneauValide=1;
-            }
-
-        }
-        console.log(`Les informations sont valides`);
-
-        // On "copie-colle" le tableau sallesDispo pour un jour et un créneau donné dans le tableau sallesDispoMobile
-       
-        k=0;
-        while (sallesDispo[iChoix][jChoix][k] != null){
-            sallesDispoMobile[k]=sallesDispo[iChoix][jChoix][k];
-            k=k+1;
-        }
-
-        // On vérifie pour chaque salle si elle est présente dans chacune des demi-heures du créneau
-        // Si ce n'est pas le cas, on remplace sa valeur par la valeur "non" dans le tableau sallesDispoMobile
-
-        i=iChoix;
-        for (j = jChoix+1; j < j2Choix+1; j++){
-            kMobile=0;
-            while (sallesDispoMobile[kMobile] != null){
-                sallePresente=0;
-                k=0;
-                while (sallesDispo[i][j][k] != null){
-                    if (sallesDispoMobile[kMobile]==sallesDispo[i][j][k]){
-                        sallePresente=1;
-                    }
-                    k=k+1;
-                }
-                if (sallePresente==0){
-                    sallesDispoMobile[kMobile]="non"
-                }
-                kMobile=kMobile+1;
+        
+        cours.jour.creneau.salle
+        
+        
+        
+        for (i=hdChoix;i<hfChoix;i++){
+            while (k!=null){
+                if (==sallesDispo[])
             }
             
         }
-
-        // Affichage des salles disponibles
-
-        console.log(`Recherche des salles disponibles... `);
-
-        kMobile=0;
-        while (sallesDispoMobile[kMobile] != null){
-            if (sallesDispoMobile[kMobile] != "non"){
-                console.log(`${sallesDispoMobile[kMobile]}`);
-                nSallesDispo=nSallesDispo+1;
-            }
-            kMobile=kMobile+1;
-        }
-        if (nSallesDispo==0){
-            console.log(`Aucune salle n'est disponible.`);
-        }
-
-    }
 
 // -----------------------------------------------------------------------------------------------------
 
